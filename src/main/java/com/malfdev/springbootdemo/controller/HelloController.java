88c00624a3cbd95afe510223225b66e1beb49683
cutema <cutemalinfei@outlook.com>
2022-10-09 21:48:47 +0800

    2022-10-09 21:48:00

--- src/main/java/com/malfdev/springbootdemo/controller/HelloController.java
+++ src/main/java/com/malfdev/springbootdemo/controller/HelloController.java
@@ -1,7 +1,7 @@
 package com.malfdev.springbootdemo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -9,5 +9,26 @@ public class HelloController {
     public String handle1(){
         System.out.println("到达controller");
         return "/success.html";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/onlyGet")
+    public String handle4(){
+        System.out.println("请求handle4方法");
+        return "请求handle4方法";
+    }
+
+    @ResponseBody
+    @PostMapping(value = "/onlyPost")
+    public String handle5(){
+        System.out.println("请求handle5方法");
+        return "请求handle5方法";
+    }
+
+    @ResponseBody
+    @PostMapping(value = "/getJson")
+    public String handle6(@RequestBody String str){
+        System.out.println("请求handle5方法"+str);
+        return "请求handle5方法";
     }
 }
